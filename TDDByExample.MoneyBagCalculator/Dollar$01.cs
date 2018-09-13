b6e0a2e6a0b1d3ba0d16d62bebebf4d631b378b6
Shamil Salakhetdinov <shamil503@hotmail.com>
2018-09-13 22:26:32 +0300

    Chapter 2: 'Degenerate Objects' completed.

---
+++ TDDByExample.MoneyBagCalculator/Dollar$01.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TDDByExample.MoneyBagCalculator
+{
+    ///<summary>
+    /// Done: 
+    ///   + 01. Multi-Currency Money
+    ///     * No class “Dollar”
+    ///     * No constructor
+    ///     * No method “times(int)”
+    ///     * No field “amount”
+    ///     $5 * 2 = $10
+    /// </summary>
+    public class Dollar01
+    {
+        public int Amount { get; private set; }
+        public Dollar01(int amount)
+        {
+            this.Amount = amount;
+        }
+        public void Times(int multiplier)
+        {
+            this.Amount *= multiplier;
+        }
+    }
+}
---
+++ TDDByExample.MoneyBagCalculator/Dollar$02.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TDDByExample.MoneyBagCalculator
+{
+    ///<summary>
+    /// Done: 
+    ///   + 01. Multi-Currency Money
+    ///     * No class “Dollar”
+    ///     * No constructor
+    ///     * No method “times(int)”
+    ///     * No field “amount”
+    ///     $5 * 2 = $10
+    ///   +02. Degenerate Objects
+    ///     Dollar side-effects?
+    ///</summary>
+    public class Dollar02
+    {
+        public int Amount { get; private set; }
+        public Dollar02(int amount)
+        {
+            this.Amount = amount;
+        }
+        public Dollar02 Times(int multiplier)
+        {
+            return new Dollar02(this.Amount * multiplier);
+        }
+    }
+}
