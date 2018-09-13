0f31b26ab104cbfddac8f1663cc447ad76c8e11a
Shamil Salakhetdinov <shamil503@hotmail.com>
2018-09-13 18:11:31 +0300

    Added program.cs to TDDByExample.NotesAndDocs.csproj

---
+++ TDDByExample.NotesAndDocs/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TDDByExample.NotesAndDocs
+{
+    class Program
+    {
+        // it;s here just to have it running under Azure Pipelines
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
