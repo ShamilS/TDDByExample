1879d46d5487e8f3d0730037b64b87d24ce137a9
Shamil Salakhetdinov <shamil503@hotmail.com>
2018-09-13 23:49:21 +0300

    Chaper 06: 'Equality for All, Redux' completed.

---
+++ TDDByExample.MoneyBagCalculator/Dollar$06.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TDDByExample.MoneyBagCalculator
+{
+    /// <summary>
+    /// Done:
+    ///    Subclassed from Money 
+    /// </summary>
+    public class Dollar06 : Money
+    {
+        public Dollar06(int amount) : base(amount) { }
+        public Dollar06 Times(int multiplier) => new Dollar06(this.Amount * multiplier);
+    }
+}
---
+++ TDDByExample.MoneyBagCalculator/Franc06.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDDByExample.MoneyBagCalculator
+{
+    /// <summary>
+    /// Done:
+    ///    Subclassed from Money 
+    /// </summary>
+    public class Franc06:Money
+    {
+        public Franc06(int amount) : base(amount) { }
+        public Franc06 Times(int multiplier) => new Franc06(this.Amount * multiplier);
+    }
+}
