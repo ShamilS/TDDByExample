9bdb29ca175b2a6d5356d1872bffbc3420576e1a
Shamil Salakhetdinov <shamil503@hotmail.com>
2018-09-13 23:26:52 +0300

    Chapter 05: "Franc-ly Speaking completed

---
+++ TDDByExample.MoneyBagCalculator/Franc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDDByExample.MoneyBagCalculator
+{
+    /// <summary>
+    /// Done: 
+    ///   +05.
+    ///     5 CHF * 2 = 10 CHF 
+    /// </summary>
+    public class Franc
+    {
+        private int Amount { get; set; }
+        public Franc(int amount)
+        {
+            this.Amount = amount;
+        }
+        public Franc Times(int multiplier) => new Franc(this.Amount * multiplier);
+        public override bool Equals(object obj) => ((Franc)obj).Amount == this.Amount;
+    }
+}
