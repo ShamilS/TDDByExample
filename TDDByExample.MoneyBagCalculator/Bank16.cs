26b388402b4ad84f530fba8ccbe40b55f89ff99d
Shamil Salakhetdinov <shamil503@hotmail.com>
2018-09-15 00:59:06 +0300

    Chapter 16: 'Abstraction, Finally' completed.

---
+++ TDDByExample.MoneyBagCalculator/Bank16.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDDByExample.MoneyBagCalculator.Chapter16
+{
+    public class Bank
+    {
+        private Hashtable rates = new Hashtable();
+        public void AddRate(String from, String to, int rate)
+        {
+            rates.Add(new Pair(from, to), rate);
+        }
+
+        public int Rate(String from, String to)
+        {
+            if (from.Equals(to)) return 1;
+            int rate = (int)rates[new Pair(from, to)];
+            return rate;
+        }
+
+        public Money Reduce(IExpression source, String to) => source.Reduce(this, to);
+    }
+}
---
+++ TDDByExample.MoneyBagCalculator/Dollar$16.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TDDByExample.MoneyBagCalculator.Chapter16
+{
+    // it's the same as in Chapter11 and Chapter12
+    public class Dollar : Money
+    {
+        public Dollar(int amount, string currency) : base(amount, currency) { }
+    }
+}
